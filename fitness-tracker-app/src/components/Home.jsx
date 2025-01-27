191b020317eac3d56ffee180f0600eb4119e4708
QuiseB29 <160094863+QuiseB29@users.noreply.github.com>
2025-01-26 21:01:19 -0600

    Update Home.jsx

--- fitness-tracker-app/src/components/Home.jsx
+++ fitness-tracker-app/src/components/Home.jsx
@@ -3,6 +3,12 @@ function Homepage() {
         <div>
             <h1>Welcome to Our Fitness Tracker</h1>
             <p>All you need to excel your fitness </p>
+            <img src="https://res.cloudinary.com/sagacity/image/upload/c_crop,h_3028,w_2422,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/Photo_Nov_26__8_52_37_AM__1__w1s0so.jpg"/>
+            <img src="https://www.yanrefitness.com/wp-content/uploads/2022/05/7-14.jpg" />
+            <img src="https://youfit.com/wp-content/uploads/elementor/thumbs/les-mills-grit-pzjqzwpshr3nvs49tegpgb8jfoi3cmnymrs1nip4ds.jpg"/>
+            <h5>
+                TRACK YOUR PROGRESS
+            </h5>
         </div>
     )
 }
